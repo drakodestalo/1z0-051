a4a092795b2d2e21c785fe28858af3ae20edb0f9
Iron Drake <irondrake@icloud.com>
2015-03-07 08:57:34 -0600

    Se corrige el grado en job_grades

--- Cap06/job_grades.sql
+++ Cap06/job_grades.sql
@@ -6,9 +6,11 @@ create table job_grades
 
 insert into job_grades values ('A', 1000, 2999);
 insert into job_grades values ('B', 3000, 5999);
-insert into job_grades values ('A', 6000, 9999);
-insert into job_grades values ('A', 10000, 13999);
-insert into job_grades values ('A', 15000, 24999);
-insert into job_grades values ('A', 25000, 40000);
+insert into job_grades values ('C', 6000, 9999);
+insert into job_grades values ('D', 10000, 13999);
+insert into job_grades values ('E', 15000, 24999);
+insert into job_grades values ('F', 25000, 40000);
 
 commit;
+
+
