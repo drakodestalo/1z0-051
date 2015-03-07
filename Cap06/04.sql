1be2701d8d23119166019195f12bba58a680d7a5
Iron Drake <irondrake@icloud.com>
2015-03-07 09:17:04 -0600

    Se agrega parcial el ejercicio 4

---
+++ Cap06/04.sql
@@ -0,0 +1,6 @@
+-- Obtener el total de empleados por departamento
+-- Contabilizar los departamentos que no tengan empleados
+-- Outer join
+
+select department_name, count(employee_id) as total
+from <>
